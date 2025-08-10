ee25cab3d3439611239a1147f68a3a5761720588
ricardo0129 <ruizr274@gmail.com>
2025-08-10 18:10:21 -0400

    refactor

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+int main() {
+    stdio_init_all();
+    printf("Hello, World!\n");
+    return 0;
+}
