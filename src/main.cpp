84b821ec47ec1d1c339b7aab69162aeb93ae0f2f
ricardo0129 <ruizr274@gmail.com>
2025-08-16 23:06:12 -0400

    repo now holds keyboard firmware code

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,184 @@
 #include <unordered_map>
 #include <string.h>
 #include <time.h>
-
 #include "pico_net/mylib.h"
-#include "pico_net/pico_net.h"
+
 #include "pico/stdlib.h"
-#include "temp_sensor/pico_temp.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "bsp/board_api.h"
+#include "tusb.h"
+
+#include "usb_descriptors.h"
+
+void hid_task(void);
+void init_tusb() {
+    board_init();
+    // init device stack on configured roothub port
+    tusb_rhport_init_t dev_init = {
+        .role = TUSB_ROLE_DEVICE,
+        .speed = TUSB_SPEED_AUTO
+    };
+    tusb_init(BOARD_TUD_RHPORT, &dev_init);
+
+    if (board_init_after_tusb) {
+        board_init_after_tusb();
+    }
+}
+
+uint8_t const conv_table[128][2] =  { HID_ASCII_TO_KEYCODE };
+ 
 
 
-void pico_net::run_tcp_client_test(void) {
-    TCP_CLIENT_T *state = tcp_client_init();
-    if (!state) {
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void) {
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void) {
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en) {
+    (void) remote_wakeup_en;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void) {
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+static uint32_t cc = 0;
+
+static void send_hid_report(uint8_t report_id, uint32_t btn) {
+    // skip if hid is not ready yet
+    if (!tud_hid_ready()) 
         return;
+
+    // use to avoid send multiple consecutive zero report for keyboard
+    static bool has_keyboard_key = false;
+
+    if(btn) {
+        uint8_t keycode[6] = { 0 };
+        uint8_t modifier   = 0;
+        uint8_t chr = 'Z';
+        if ( conv_table[chr][0] ) modifier = KEYBOARD_MODIFIER_LEFTSHIFT;
+        keycode[0] = conv_table[chr][1];
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, modifier, keycode);
+        //tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+        has_keyboard_key = true;
+    } 
+    else {
+        // send empty key report if previously has key pressed
+        if (has_keyboard_key) 
+            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+        has_keyboard_key = false;
     }
-    if (!tcp_client_open(state)) {
-        tcp_result(state, -1);
-        return;
+}
+
+
+// Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
+// tud_hid_report_complete_cb() is used to send the next report after previous one is complete
+void hid_task(void) {
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+    static uint32_t touch_ms = 0;
+    static bool touch_state = false;
+    if(board_millis() - start_ms < interval_ms) 
+        return; // not enough time
+    start_ms += interval_ms;
+
+    uint32_t const btn = board_button_read();
+    // Remote wakeup
+    if(tud_suspended() && btn) {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
     }
-    while (!state->complete) {
-        /*
-        */
-        printf("Polling...\n");
-        cyw43_arch_poll();
-        cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
-        //sleep_ms(2000);
+    else {
+        // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
+        send_hid_report(REPORT_ID_KEYBOARD, btn);
     }
-    free(state);
+}
+
+// Invoked when sent REPORT successfully to host
+// Application can use this to send the next report
+// Note: For composite reports, report[0] is report ID
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len) {
+    (void) instance;
+    (void) len;
+    uint8_t next_report_id = report[0] + 1u;
+
+    if (next_report_id < REPORT_ID_COUNT) {
+        send_hid_report(next_report_id, board_button_read());
+    }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    // TODO not Implemented
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+    (void) instance;
+
+    /*
+    if(report_type == HID_REPORT_TYPE_OUTPUT) {
+    // Set keyboard LED e.g Capslock, Numlock etc...
+        if (report_id == REPORT_ID_KEYBOARD) {
+        // bufsize should be (at least) 1
+            if( bufsize < 1 ) 
+                return;
+            uint8_t const kbd_leds = buffer[0];
+            if(kbd_leds & KEYBOARD_LED_CAPSLOCK) {
+                // Capslock On: disable blink, turn led on
+                blink_interval_ms = 0;
+                board_led_write(true);
+            }
+            else {
+                // Caplocks Off: back to normal blink
+                board_led_write(false);
+                blink_interval_ms = BLINK_MOUNTED;
+            }
+        }
+    }
+    */
 }
 
 int main() {
     stdio_init_all();
+    init_tusb();
 
-    gpio_init(DHT_PIN);
+
+    while(1) {
+        //tud_task(); // tinyusb device task
+        //hid_task();
+        printf("working...");
+        sleep_ms(1000); // sleep for 1 second
+    }
 
     const std::unordered_map<std::string, greeter::LanguageCode> languages{
         {"en", greeter::LanguageCode::EN},
@@ -45,33 +192,5 @@ int main() {
 
     greeter::Greeter greeter("RICKY");
     printf("{}\n", greeter.greet(langIt->second));
-    /*
-
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return 1;
-    }
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } 
-    else {
-        printf("Connected.\n");
-    }
-    pico_net::run_tcp_client_test();
-    cyw43_arch_deinit();
-    */
-
-    while(true) {
-        dht_reading reading;
-        read_from_dht(&reading);
-        sleep_ms(2000);
-    }
-    //float fahrenheit = (reading.temp_celsius * 9 / 5) + 32;
-    //printf("Humidity = %.1f%%, Temperature = %.1fC (%.1fF)\n",
-    //       reading.humidity, reading.temp_celsius, fahrenheit);
     return 0;
 }
