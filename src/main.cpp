aeb95b4520c0bcc73963aecf821030baa9764946
ricardo0129 <ruizr274@gmail.com>
2025-08-18 00:05:18 -0400

    left side minimally working

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,41 @@
 
 #include "usb_descriptors.h"
 
+/* Pinout
+ * Bat  RGB
+ * GND  DATA
+ * RST  GND
+ * VCC  GND
+ * LR   SDA
+ * R1   SCL
+ * R0   C1
+ * R2   C2
+ * R3   C3
+ * E2A  C4
+ * E2B  E1B
+ * C0   E1A
+*/
+const int LED_PIN = 17;
+
+const int COLS = 5;
+const int ROWS = 4;
+
+const int ROW_TO_PIN[4] = {27, 28, 26, 22};
+const int COL_TO_PIN[5] = {21, 4, 5, 6, 7};
+
+struct KeyState {
+    bool is_pressed;
+    uint64_t last_changed;
+};
+
+KeyState key_states[ROWS][COLS] = {0};
+
+char left_layout[3][5] = {
+    {'T', 'R', 'E', 'W', 'Q'},
+    {'G', 'F', 'D', 'S', 'A'},
+    {'B', 'V', 'C', 'X', 'Z'},
+};
+
 void hid_task(void);
 void init_tusb() {
     board_init();
@@ -60,31 +95,55 @@ void tud_resume_cb(void) {
 // USB HID
 //--------------------------------------------------------------------+
 static uint32_t cc = 0;
+const int MAX_KEY_COUNT = 6;
+const uint64_t DEBOUNCE_US = 5000;
+const uint64_t KEY_HOLD_US = 300000; // 300ms
 
-static void send_hid_report(uint8_t report_id, uint32_t btn) {
+static void send_hid_report(uint8_t report_id) {
     // skip if hid is not ready yet
     if (!tud_hid_ready()) 
         return;
+    static bool changed = false;
+    uint8_t keycode[MAX_KEY_COUNT] = { 0 };
+    uint8_t key_count = 0;
+    uint8_t modifier  = 0;
 
-    // use to avoid send multiple consecutive zero report for keyboard
-    static bool has_keyboard_key = false;
-
-    if(btn) {
-        uint8_t keycode[6] = { 0 };
-        uint8_t modifier   = 0;
-        uint8_t chr = 'Z';
-        if ( conv_table[chr][0] ) modifier = KEYBOARD_MODIFIER_LEFTSHIFT;
-        keycode[0] = conv_table[chr][1];
-        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, modifier, keycode);
-        //tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-        has_keyboard_key = true;
-    } 
-    else {
-        // send empty key report if previously has key pressed
-        if (has_keyboard_key) 
-            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
-        has_keyboard_key = false;
+    for(int i = 0; i < ROWS; i++) {
+        int row_pin = ROW_TO_PIN[i];
+        gpio_put(row_pin, 1);
+        sleep_ms(1);
+        for(int j = 0; j < COLS; j++) {
+            int col_pin = COL_TO_PIN[j];
+            int state = gpio_get(col_pin);
+            uint64_t now = time_us_64();
+            if(state != key_states[i][j].is_pressed && now - key_states[i][j].last_changed > DEBOUNCE_US) {
+                key_states[i][j].is_pressed = state;
+                key_states[i][j].last_changed = now;
+                uint8_t chr = left_layout[i][j];
+                if( conv_table[chr][0] ) modifier = KEYBOARD_MODIFIER_LEFTSHIFT;
+                if(state && key_count < MAX_KEY_COUNT) {
+                    keycode[key_count++] = conv_table[chr][1];
+                    changed = true;
+                }
+            }
+            else if(state && now - key_states[i][j].last_changed > KEY_HOLD_US && key_count < MAX_KEY_COUNT) {
+                uint8_t chr = left_layout[i][j];
+                if( conv_table[chr][0] ) modifier = KEYBOARD_MODIFIER_LEFTSHIFT;
+                keycode[key_count++] = conv_table[chr][1];
+                changed = true;
+            }
+        }
+        gpio_put(row_pin, 0);
     }
+    if(changed) {
+        if(key_count) {
+            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, modifier, keycode);
+        }
+        else {
+            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+        }
+    }
+
 }
 
 
@@ -100,16 +159,17 @@ void hid_task(void) {
         return; // not enough time
     start_ms += interval_ms;
 
-    uint32_t const btn = board_button_read();
+    //uint32_t const btn = board_button_read();
+
     // Remote wakeup
-    if(tud_suspended() && btn) {
+    if(tud_suspended()) {
         // Wake up host if we are in suspend mode
         // and REMOTE_WAKEUP feature is enabled by host
         tud_remote_wakeup();
     }
     else {
         // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
-        send_hid_report(REPORT_ID_KEYBOARD, btn);
+        send_hid_report(REPORT_ID_KEYBOARD);
     }
 }
 
@@ -121,9 +181,11 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_
     (void) len;
     uint8_t next_report_id = report[0] + 1u;
 
+    /*
     if (next_report_id < REPORT_ID_COUNT) {
-        send_hid_report(next_report_id, board_button_read());
+        send_hid_report(next_report_id);
     }
+    */
 }
 
 // Invoked when received GET_REPORT control request
@@ -168,16 +230,57 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
     */
 }
 
+//map<int,int> rows_in_col[COLS];
+void init_keyboard_pins() {
+
+    /*
+    while(true) {
+        if(value == 1) {
+        }
+        sleep_ms(1);
+    }
+    */
+}
+
+void scan_matrix() {
+    init_keyboard_pins();
+}
+
 int main() {
     stdio_init_all();
+
+    for(int i = 0; i < ROWS; i++) {
+        for(int j = 0; j < COLS; j++) {
+            key_states[i][j].is_pressed = false;
+            key_states[i][j].last_changed = time_us_64();
+        }
+    }
+
+    // Turn on LED to indicate we are running
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    for(int i = 0; i < COLS; i++) {
+        int pin = COL_TO_PIN[i];
+        gpio_init(pin);
+        //gpio_set_dir(pin, GPIO_OUT);
+        gpio_set_dir(pin, GPIO_IN);
+    }
+    for(int i = 0; i < ROWS; i++) {
+        int pin = ROW_TO_PIN[i];
+        gpio_init(pin);
+        //gpio_set_dir(pin, GPIO_IN);
+        gpio_set_dir(pin, GPIO_OUT);
+    }
+
+
     init_tusb();
 
 
     while(1) {
-        //tud_task(); // tinyusb device task
-        //hid_task();
-        printf("working...");
-        sleep_ms(1000); // sleep for 1 second
+        tud_task(); // tinyusb device task
+        hid_task();
+        sleep_ms(10); // sleep for 1 second
     }
 
     const std::unordered_map<std::string, greeter::LanguageCode> languages{
