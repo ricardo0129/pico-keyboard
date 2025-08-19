340d672c244620acef5f1f64dae173fe9b861b3e
ricardo0129 <ruizr274@gmail.com>
2025-08-18 23:42:01 -0400

    i2c working but not really

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,220 @@
  * E2B  E1B
  * C0   E1A
 */
+
+
+/*
+ * Copyright (c) 2021 Valentin Milea <valentin.milea@gmail.com>
+ * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <hardware/i2c.h>
+#include <pico/i2c_slave.h>
+#include <pico/stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static const uint I2C_SLAVE_ADDRESS = 0x17;
+static const uint I2C_BAUDRATE = 100000; // 100 kHz
+
+#ifdef i2c_default
+// For this example, we run both the master and slave from the same board.
+// You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
+static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
+static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
+static const uint I2C_MASTER_SDA_PIN = 6;
+static const uint I2C_MASTER_SCL_PIN = 7;
+
+enum class State {
+    RETURN_LENGTH,
+    READ_REQUESTED_LENGTH,
+    WRITE_REQUESTED_LENGTH
+};
+const int MAX_QUEUE_SIZE = 256;
+const int MAX_BUFFER_SIZE = 32;
+struct stack {
+    uint8_t data[256];
+    uint8_t head;
+    stack() : head(0) {
+    }
+    int push(uint8_t value) {
+        if(head >= MAX_QUEUE_SIZE) {
+            return -1; // stack is full
+        }
+        data[head++] = value;
+    }
+    int pop() {
+        if(head <= 0) {
+            hard_assert(false, "Queue is empty");
+        }
+        return data[head--];
+    }
+    int size() {
+        return head;
+    }
+};
+
+// The slave implements a 256 byte memory. To write a series of bytes, the master first
+// writes the memory address, followed by the data. The address is automatically incremented
+// for each byte transferred, looping back to 0 upon reaching the end. Reading is done
+// sequentially from the current memory address.
+static struct {
+    stack mem; // memory stack
+    State current_state = State::RETURN_LENGTH;
+    uint8_t write_remaining = 0; // number of bytes remaining to be written
+} context;
+
+// Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
+// printing to stdio may interfere with interrupt handling.
+static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    switch (event) {
+        case I2C_SLAVE_RECEIVE: // master has written some data
+            //Master has made a request for data
+            context.write_remaining = i2c_read_byte_raw(i2c);
+            if(context.current_state == State::READ_REQUESTED_LENGTH) {
+                if(context.write_remaining == 0) {
+                    //We can skip the next state
+                    context.current_state = State::RETURN_LENGTH;
+                } 
+                else {
+                    context.current_state = State::WRITE_REQUESTED_LENGTH;
+                }
+            }
+            else {
+                // This should never happen
+                hard_assert(false, "Unexpected state in I2C slave handler");
+            }
+            break;
+        case I2C_SLAVE_REQUEST: // master is requesting data
+            if(context.current_state == State::RETURN_LENGTH) {
+                // Master is requestion the size of the memory
+                i2c_write_byte_raw(i2c, context.mem.size());
+                context.current_state = State::READ_REQUESTED_LENGTH;
+            }
+            else if(context.current_state == State::WRITE_REQUESTED_LENGTH) {
+                // Master is requesting data
+                //hard_assert(context.write_remaining > 0, "Requested size already zero");
+                //hard_assert(context.mem.size() > 0, "Memory is empty");
+                // Write the next byte from memory
+                i2c_write_byte_raw(i2c, context.mem.pop());
+                context.write_remaining--;
+            }
+            else {
+                // This should never happen
+                hard_assert(false, "Unexpected state in I2C slave handler");
+            }
+            break;
+        case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+            //Clean up the state
+            context.current_state = State::RETURN_LENGTH;
+            context.write_remaining = 0;
+            break;
+        default:
+            break;
+    }
+}
+
+static void setup_slave() {
+    gpio_init(I2C_SLAVE_SDA_PIN);
+    gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SDA_PIN);
+
+    gpio_init(I2C_SLAVE_SCL_PIN);
+    gpio_set_function(I2C_SLAVE_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SLAVE_SCL_PIN);
+
+    i2c_init(i2c0, I2C_BAUDRATE);
+    // configure I2C0 for slave mode
+    i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
+}
+
+static void run_master() {
+    gpio_init(I2C_MASTER_SDA_PIN);
+    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
+    // pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
+    gpio_pull_up(I2C_MASTER_SDA_PIN);
+
+    gpio_init(I2C_MASTER_SCL_PIN);
+    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_MASTER_SCL_PIN);
+
+    i2c_init(i2c1, I2C_BAUDRATE);
+
+    /*
+    for (uint8_t mem_address = 0;; mem_address = (mem_address + 32) % 256) {
+        char msg[32];
+        snprintf(msg, sizeof(msg), "Hello, I2C slave! - 0x%02X", mem_address);
+        uint8_t msg_len = strlen(msg);
+
+        uint8_t buf[32];
+        buf[0] = mem_address;
+        memcpy(buf + 1, msg, msg_len);
+        // write message at mem_address
+        printf("Write at 0x%02X: '%s'\n", mem_address, msg);
+        int count = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1 + msg_len, false);
+        if (count < 0) {
+            puts("Couldn't write to slave, please check your wiring!");
+            return;
+        }
+        hard_assert(count == 1 + msg_len);
+
+        // seek to mem_address
+        count = i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, true);
+        hard_assert(count == 1);
+        // partial read
+        uint8_t split = 5;
+        count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, split, true);
+        hard_assert(count == split);
+        buf[count] = '\0';
+        printf("Read  at 0x%02X: '%s'\n", mem_address, buf);
+        hard_assert(memcmp(buf, msg, split) == 0);
+        // read the remaining bytes, continuing from last address
+        count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, msg_len - split, false);
+        hard_assert(count == msg_len - split);
+        buf[count] = '\0';
+        printf("Read  at 0x%02X: '%s'\n", mem_address + split, buf);
+        hard_assert(memcmp(buf, msg + split, msg_len - split) == 0);
+
+        puts("");
+        sleep_ms(2000);
+    }
+    */
+    uint8_t buf[MAX_BUFFER_SIZE];
+    while(true) {
+        printf("Requesting length from slave...\n");
+        int count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, false);
+        if (count < 0) {
+            puts("Couldn't read from slave, please check your wiring!");
+            return;
+        }
+        hard_assert(count == 1, "Expected to read 1 byte");
+        uint8_t requested_length = buf[0];
+        hard_assert(requested_length <= MAX_BUFFER_SIZE, "Requested length exceeds buffer size");
+        printf("Requesting %d bytes from slave...\n", requested_length);
+        if(requested_length > 0) {
+            printf("writing\n");
+            i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, false);
+            printf("Reading %d bytes from slave...\n", requested_length);
+            //read all the requested bytes
+            count = i2c_read_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, requested_length, true);
+            hard_assert(count == requested_length, "Expected to read %d bytes, got %d", requested_length, count);
+        }
+        else {
+            printf("Requested length is zero, nothing to write\n");
+            i2c_write_blocking(i2c1, I2C_SLAVE_ADDRESS, buf, 1, true);
+            printf("Done writing\n");
+        }
+        sleep_ms(2000);
+    }
+}
+#endif
+
+
+
+
+
 const int LED_PIN = 17;
 
 const int COLS = 5;
@@ -246,8 +460,27 @@ void scan_matrix() {
     init_keyboard_pins();
 }
 
+
 int main() {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c / slave_mem_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+    return 0;
+#else
+    puts("\nI2C slave example");
+    gpio_put(LED_PIN, 1);
+
+    setup_slave();
+    run_master();
+#endif
+    while(true) {
+        sleep_ms(1000);
+    }
+    return 0;
 
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLS; j++) {
@@ -257,8 +490,6 @@ int main() {
     }
 
     // Turn on LED to indicate we are running
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
 
     for(int i = 0; i < COLS; i++) {
         int pin = COL_TO_PIN[i];
